a884a79edbf5bbd39e27d8a23ad01682682c736f
Richard Wang <richardwang007@gmail.com>
2025-04-08 02:53:49 -0700

    clean

---
+++ src/gittercommitter/llm_interface.py
@@ -0,0 +1,44 @@
+import openai
+import os
+from string import Template
+from google import genai
+from importlib.resources import files
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+# dont forget to remove keys when pushing
+GEMINI_API_KEY = "" # https://aistudio.google.com/apikey
+
+def load_prompt_template():
+    prompt_path = files("gittercommitter.prompts").joinpath("basic_PR_template.txt")
+    with prompt_path.open("r") as f:
+        return Template(f.read())
+
+def summarize_diff_openai(diff_text):
+    template = load_prompt_template()
+    filled_prompt = template.substitute(git_diff=diff_text)
+
+    # don't run this shit - take it out and add llama for free
+    response = openai.ChatCompletion.create(
+        model="gpt-4-turbo",
+        messages=[
+            {"role": "system", "content": "You are a software engineer skilled in code reviews."},
+            {"role": "user", "content": filled_prompt}
+        ],
+        temperature=0.2
+    )
+    return response['choices'][0]['message']['content']
+
+def summarize_diff_gemini(diff_text):
+    template = load_prompt_template()
+    filled_prompt = template.substitute(git_diff=diff_text)    
+
+    client = genai.Client(api_key=GEMINI_API_KEY)
+
+    content = [filled_prompt]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents=content
+    )
+    # print(response.__dict__.keys())
+    return response.text
+
